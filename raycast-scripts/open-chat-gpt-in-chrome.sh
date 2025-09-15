275382cb1143592fc2a88aecfa083901068cd4d8
Max Schnur <max.schnur@gmail.com>
2025-09-15 12:46:35 -0400

    Add script and icon for 'Open ChatGPT in Chrome'.

---
+++ raycast-scripts/open-chat-gpt-in-chrome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# @raycast.schemaVersion 1
+# @raycast.title ChatGPT in Chrome
+# @raycast.mode silent
+# @raycast.icon chatgpt-white-on-black-circle-background.png
+
+osascript ~/bin/open-url-in-chrome.applescript "https://chatgpt.com"
